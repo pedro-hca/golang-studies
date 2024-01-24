c44ebd1b574a54d853a729e07153a352cca07496
pedro-hca <pedroandradecontato@hotmail.com>
2024-01-23 22:19:09 -0300

    creating a sql database

---
+++ 01-go-the-complete-guide/09-rest-api/db/db.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+	createTables()
+}
+
+func createTables() {
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		user_id INTEGER
+	)
+	`
+	_, err := DB.Exec(createEventsTable)
+	if err != nil {
+		panic(fmt.Sprintf("Could not create events table: %v", err))
+	}
+}
