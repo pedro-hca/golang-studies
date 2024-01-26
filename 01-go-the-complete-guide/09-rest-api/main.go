7b957933f78c274a60f586f903206e156447efb3
pedro-hca <pedroandradecontato@hotmail.com>
2024-01-26 20:59:52 -0300

    creating endpoint GetEventById

--- 01-go-the-complete-guide/09-rest-api/main.go
+++ 01-go-the-complete-guide/09-rest-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/pedro-hca/go-studies/09-rest-api/db"
@@ -11,11 +12,25 @@ import (
 func main() {
 	db.InitDB()
 	server := gin.Default()
-	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+	server.GET("/events", getEvents)    // GET, POST, PUT, PATCH, DELETE
+	server.GET("/events/:id", getEvent) // /events/1, /5, /6
 	server.POST("/events", createEvent)
 	server.Run(":8080") //localhost 8080
 }
 
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not parse id."})
+		return
+	}
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+}
 func getEvents(context *gin.Context) {
 	events, err := models.GetAllEvents()
 	if err != nil {
