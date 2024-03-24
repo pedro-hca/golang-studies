09d78712fcaa7f6ce617cf30326c1eea7e86c2e3
pedro-hca <pedroandradecontato@hotmail.com>
2024-03-23 23:26:22 -0300

    feat: add services

--- 02-microservice/application/services/upload_manager.go
+++ 02-microservice/application/services/upload_manager.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -62,34 +63,53 @@ func (vu *VideoUpload) loadPaths() error {
 }
 
 func (vu *VideoUpload) ProcessUpload(concurrency int, doneUpload chan string) error {
-	in := make(chan int, runtime.NumCPU())
-	returnChannel := make(chan string)
+	in := make(chan int, runtime.NumCPU()) // qual o arquivo baseado na posicao do slice Paths
+	returnChannel := make(chan string)     // retorno do canal caso tenha gerado erro
 
-	err := vu.loadPaths()
+	err := vu.loadPaths() // carrega todos os paths
 	if err != nil {
 		return err
 	}
 
-	uploadClient, ctx, err := getClientUpload()
+	uploadClient, ctx, err := getClientUpload() // instancia o client do upload
 	if err != nil {
 		return err
 	}
-	for process := 0; process < concurrency; process++ {
+
+	for process := 0; process < concurrency; process++ { //inicia o numero de workers desejado de acordo com a concurrency passada como parametro
 		go vu.uploadWorker(in, returnChannel, uploadClient, ctx)
 	}
 
-	go func() {
+	go func() { //goroutine que alimenta o canal in para que seja lido e esvaziado na funçao uploadWorker
 		for x := 0; x < len(vu.Paths); x++ {
 			in <- x
 		}
 		close(in)
 	}()
+
+	for r := range returnChannel { //for para caso retorne um erro em returnChan parar a execução das goroutines
+		if r != "" {
+			doneUpload <- r
+			break
+		}
+	}
+
+	return nil
 }
 
 func (vu *VideoUpload) uploadWorker(in chan int, returnChan chan string, uploadClient *storage.Client, ctx context.Context) {
-	for x := range in {
 
+	for x := range in { // for para ler o conteudo do canal in e esvaziar fazendo o upload por meio da funçao UploadObject, uma vez esvaziado o canal a goroutine da funçao acima enche novamente
+		err := vu.UploadObject(vu.Paths[x], uploadClient, ctx)
+		if err != nil { //caso UploadObject retorne erro é passado para o returnChan que executa um break nas goroutines
+			vu.Errors = append(vu.Errors, vu.Paths[x])
+			log.Printf("error during the upload: %v. Error: %v", vu.Paths[x], err)
+			returnChan <- err.Error()
+		}
+
+		returnChan <- ""
 	}
+	returnChan <- "upload completed"
 }
 
 func getClientUpload() (*storage.Client, context.Context, error) {
---
+++ 02-microservice/application/services/upload_manager_test.go
@@ -0,0 +1,48 @@
+package services_test
+
+import (
+	"encoder/application/services"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func TestVideoServiceUpoad(t *testing.T) {
+
+	video, repo := prepare()
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("encodertest")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+	require.Nil(t, err)
+
+	videoUpload := services.NewVideoUpload()
+	videoUpload.OutputBucket = "videotest"
+	videoUpload.VideoPath = os.Getenv("localStoragePath") + "/" + video.ID
+
+	doneUpload := make(chan string)
+	go videoUpload.ProcessUpload(50, doneUpload)
+
+	result := <-doneUpload
+	require.Equal(t, result, "upload completed")
+
+	err = videoService.Finish()
+	require.Nil(t, err)
+}
