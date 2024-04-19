382711f8aa7801773a3ea2757774e5de2c286b9d
pedro-hca <pedroandradecontato@hotmail.com>
2024-04-19 16:11:08 -0300

    feat: add fanin fanout goroutine pattern

---
+++ 06-parquet-converter/internal/pkg/parquet/fan_in.go
@@ -0,0 +1,22 @@
+package parquet
+
+import (
+	"sync"
+
+	"github.com/apache/arrow/go/v16/arrow"
+)
+
+func FanIn(chans ...<-chan []arrow.Record) <-chan []arrow.Record {
+	out := make(chan []arrow.Record)
+	wg := &sync.WaitGroup{}
+	wg.Add(len(chans))
+	for _, c := range chans {
+		go func(arrow <-chan []arrow.Record) {
+			for rec := range arrow {
+				out <- rec
+			}
+			wg.Done()
+		}(c)
+	}
+	return out
+}
